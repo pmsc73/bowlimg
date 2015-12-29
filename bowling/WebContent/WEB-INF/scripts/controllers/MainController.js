017b0d5a054c1af38638be459af37296060e7908
Scott-Young-6746 <sy6746@gmail.com>
2015-12-28 21:47:22 -0330

    Method stubs for onClick functions added to MainController

--- bowling/WebContent/WEB-INF/scripts/controllers/MainController.js
+++ bowling/WebContent/WEB-INF/scripts/controllers/MainController.js
@@ -1,33 +1,8 @@
 /**
  * 
  */
-app.controller('MainController',['$scope', function($scope){
-	var a;
-	if (condition1) {
-		a = assignment_1;
-	} else if (condition2) {
-		a = assignment_2;
-	} else if (condition3) {
-		a = assignment_3;
-	} else if (condition4) {
-		a = assignment_4;
-	} else if (condition5) {
-		a = assignment_5;
-	} else if (condition6) {
-		a = assignment_6;
-	} else if (condition7) {
-		a = assignment_7;
-	} else {
-		a = assignment_8
-	}
-	//OR
-	var b = ( condition1) ? assignment_1 :
-			((condition1) ? assignment_1 :
-			((condition1) ? assignment_1 :	
-			((condition1) ? assignment_1 :
-			((condition1) ? assignment_1 :
-			((condition1) ? assignment_1 :
-			((condition1) ? assignment_1 :
-						 	assignment_1 
-						 		   ))))));
+app.controller('MainController',['$scope', '$event', function($scope, $event){
+	$scope.onClickReady = function($event){/*...*/};
+	$scope.onClickCreate = function($event){/*...*/};
+	$scope.onClickJoin = function($event){/*...*/};
 }]);
